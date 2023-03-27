be6a451856508360f62497ca3a827579d6c362cb
Larisa <kos.lv@yandex.com>
2023-03-27 17:53:49 +0200

    types basics

---
+++ sandbox.js
@@ -0,0 +1,11 @@
+var character = 'mari';
+console.log(character);
+var inputs = document.querySelectorAll('input');
+console.log(inputs);
+inputs.forEach(function (input) {
+    console.log(input);
+});
+var circ = function (diameter) {
+    return diameter = Math.PI;
+};
+console.log(circ(12));
---
+++ sandbox.ts
@@ -0,0 +1,15 @@
+const character = 'mari';
+console.log(character);
+
+const inputs = document.querySelectorAll('input');
+console.log(inputs)
+
+inputs.forEach(input => {
+    console.log(input);
+})
+
+const circ = (diameter: number) => {
+    return diameter = Math.PI;
+}
+
+console.log(circ(12));
